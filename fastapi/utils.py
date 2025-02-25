cdbca9495c73ffb6c503281d2bcbe768bec379da
herybala <herhapy@gmail.com>
2025-02-25 22:04:18 +0100

    update data api data model

--- fastapi/utils.py
+++ fastapi/utils.py
@@ -14,11 +14,196 @@ def init_db(MONGO_URI):
     contact_info_collection = db['contact_info']
 
     # Données initiales
+    # products = [
+    #     {"id": "0001", "name": "Vélo de route", "price": 800, "category": "Vélos", "stock": 10, "image_url": "http://localhost/static/images/velo_route.jpg"},
+    #     {"id": "0002", "name": "VTT", "price": 600, "category": "Vélos", "stock": 15, "image_url": "http://localhost/static/images/vtt.jpg"},
+    #     {"id": "0003", "name": "Vélo électrique", "price": 1200, "category": "Vélos", "stock": 5, "image_url": "http://localhost/static/images/velo_electrique.jpg"}
+    #     ]
     products = [
-        {"id": "0001", "name": "Vélo de route", "price": 800, "category": "Vélos", "stock": 10, "image_url": "http://localhost/static/images/velo_route.jpg"},
-        {"id": "0002", "name": "VTT", "price": 600, "category": "Vélos", "stock": 15, "image_url": "http://localhost/static/images/vtt.jpg"},
-        {"id": "0003", "name": "Vélo électrique", "price": 1200, "category": "Vélos", "stock": 5, "image_url": "http://localhost/static/images/velo_electrique.jpg"}
-        ]
+                {
+                    "description": "Vélo de route, un vélo parfait pour la route.",
+                    "handle": "velo-de-route",
+                    "title": "Vélo de route",
+                    "images": {
+                        "edges": [
+                        {
+                            "node": {
+                            "altText": "vélo de route",
+                            "height": 1000,
+                            "width": 1000,
+                            "id": "image-1",
+                            "originalSrc": "http://localhost/static/images/velo_route.jpg"
+                            }
+                        },
+                        {
+                            "node": {
+                            "altText": "vélo de route",
+                            "height": 1000,
+                            "width": 1000,
+                            "id": "image-2",
+                            "originalSrc": "http://localhost/static/images/velo_route.jpg"
+                            }
+                        },
+                        {
+                            "node": {
+                            "altText": "vélo de route",
+                            "height": 1000,
+                            "width": 1000,
+                            "id": "image-3",
+                            "originalSrc": "http://localhost/static/images/velo_route.jpg"
+                            }
+                        }
+                        ]
+                    },
+                    "variants": {
+                        "edges": [
+                        {
+                            "node": {
+                            "id": "variant-1",
+                            "price": "800",
+                            "title": "S"
+                            }
+                        },
+                        {
+                            "node": {
+                            "id": "variant-2",
+                            "price": "820",
+                            "title": "M"
+                            }
+                        },
+                        {
+                            "node": {
+                            "id": "variant-3",
+                            "price": "830",
+                            "title": "L"
+                            }
+                        }
+                        ]
+                    }
+                },
+                {
+                    "description": "Vélo tout terrain",
+                    "handle": "vtt",
+                    "title": "Vélo tout terrain",
+                    "images": {
+                        "edges": [
+                        {
+                            "node": {
+                            "altText": "vélo tout terrain",
+                            "height": 1000,
+                            "width": 1000,
+                            "id": "image-1",
+                            "originalSrc": "http://localhost/static/images/vtt.jpg"
+                            }
+                        },
+                        {
+                            "node": {
+                            "altText": "vélo tout terrain",
+                            "height": 1000,
+                            "width": 1000,
+                            "id": "image-2",
+                            "originalSrc": "http://localhost/static/images/vtt.jpg"
+                            }
+                        },
+                        {
+                            "node": {
+                            "altText": "vélo tout terrain",
+                            "height": 1000,
+                            "width": 1000,
+                            "id": "image-3",
+                            "originalSrc": "http://localhost/static/images/vtt.jpg"
+                            }
+                        }
+                        ]
+                    },
+                    "variants": {
+                        "edges": [
+                        {
+                            "node": {
+                            "id": "variant-1",
+                            "price": "800",
+                            "title": "S"
+                            }
+                        },
+                        {
+                            "node": {
+                            "id": "variant-2",
+                            "price": "820",
+                            "title": "M"
+                            }
+                        },
+                        {
+                            "node": {
+                            "id": "variant-3",
+                            "price": "830",
+                            "title": "L"
+                            }
+                        }
+                        ]
+                    }
+                },
+                {
+                    "description": "Vélo électrique",
+                    "handle": "velo_electrique",
+                    "title": "Vélo électrique",
+                    "images": {
+                        "edges": [
+                        {
+                            "node": {
+                            "altText": "vélo électrique",
+                            "height": 1000,
+                            "width": 1000,
+                            "id": "image-1",
+                            "originalSrc": "http://localhost/static/images/velo_electrique.jpg"
+                            }
+                        },
+                        {
+                            "node": {
+                            "altText": "vélo électrique",
+                            "height": 1000,
+                            "width": 1000,
+                            "id": "image-2",
+                            "originalSrc": "http://localhost/static/images/velo_electrique.jpg"
+                            }
+                        },
+                        {
+                            "node": {
+                            "altText": "vélo électrique",
+                            "height": 1000,
+                            "width": 1000,
+                            "id": "image-3",
+                            "originalSrc": "http://localhost/static/images/velo_electrique.jpg"
+                            }
+                        }
+                        ]
+                    },
+                    "variants": {
+                        "edges": [
+                        {
+                            "node": {
+                            "id": "variant-1",
+                            "price": "800",
+                            "title": "S"
+                            }
+                        },
+                        {
+                            "node": {
+                            "id": "variant-2",
+                            "price": "820",
+                            "title": "M"
+                            }
+                        },
+                        {
+                            "node": {
+                            "id": "variant-3",
+                            "price": "830",
+                            "title": "L"
+                            }
+                        }
+                        ]
+                    }
+                }
+            ]
 
     # Insérer les données si elles n'existent pas déjà
     if products_collection.count_documents({}) == 0:
